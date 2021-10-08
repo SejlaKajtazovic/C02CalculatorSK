cccbcc96a4b6d0a52bbe525a306cd1b75247aca9
skajtazovic <sejla.kajtazovic@gmail.com>
2021-10-08 20:00:50 +0200

    hah

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,23 @@
 public class Calculator {
 
     public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+
+        double b = 2;
+        System.out.println(b);
+
+        boolean c = false;
+        System.out.println(c);
+
+        char d = '3';
+        System.out.println(d);
+
+        String e = "08.10.2021";
+        System.out.println(e);
+
+
+
 
     }
 
