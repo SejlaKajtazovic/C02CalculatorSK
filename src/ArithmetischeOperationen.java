a6e2456e1085f8a363cc8aebe155021ac3443e66
skajtazovic <sejla.kajtazovic@gmail.com>
2021-10-08 20:24:02 +0200

    ArithmetischeOperationen

---
+++ src/ArithmetischeOperationen.java
@@ -0,0 +1,18 @@
+public class ArithmetischeOperationen {
+
+    public static void main(String[] args){
+        int a = 1;
+        System.out.println(a);
+
+        int b = 2;
+        System.out.println(b);
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(b/a);
+        System.out.println(a*b);
+        System.out.println(a%b);
+
+
+    }
+}
